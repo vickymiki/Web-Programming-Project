ee5eb3c8e383bd4cc9ce6bd51a0ff55507d3eca5
Matt Noga <mnoga@stevens.edu>
2021-11-20 15:17:57 -0500

    MongoDB Configuration

---
+++ config/mongoCollections.js
@@ -0,0 +1,26 @@
+const dbConnection = require('./mongoConnection');
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* List collections here: */
+//Adding everything we might need now. Need to uncomment as we use them 
+module.exports = {
+  users: getCollectionFn('users'),
+  managers: getCollectionFn('managers'),
+  //restauarants: getCollectionFn('restauarants'),
+  //food_items: getCollectionFn('food_items'),
+  //reviews: getCollectionFn('reviews'),
+  //replies: getCollectionFn('replies'),
+  //orders_placed: getCollectionFn('orders_placed'),
+};
