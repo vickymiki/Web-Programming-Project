5a5c118b6057c7c60b80bf915c1a2b7d7e1a3745
Zhixiang Fu <52135140+zxf1106@users.noreply.github.com>
2021-11-24 09:22:06 -0500

    Add files via upload

---
+++ data/index.js
@@ -0,0 +1,5 @@
+const managers = require("./managers");
+const users = require("./users");
+const restauarants = require("./restaurants");
+const reviews = require("./reviews");
+const replies = require("./replies");
