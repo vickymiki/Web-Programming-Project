94790875c0e66f687f4f45bd959045d0d77663cd
Caleb Elliott <calebpe531@gmail.com>
2021-12-04 11:52:26 -0500

    Removes unused code

--- routes/logout.js
+++ routes/logout.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const user_DAL = require('../data/users');
-const path = require('path');
 
 router.get('/', async (req, res) => {
     req.session.destroy();
